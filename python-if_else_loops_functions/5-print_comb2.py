521264bcccb3d27d1737248517073acd87927123
Pholiso Toti <84088467+Pholi658@users.noreply.github.com>
2025-12-14 22:34:24 +0200

    Refactor print_comb2.py for cleaner output

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-for x in range(100):
-    if x < 99:
-        print("{:02d}, ".format(x), end="")
+for x in range(0, 100):
+    if x == 99:
+        print("{}" .format(x))
     else:
-        print("{:02d}".format(x))
-print("\n")
+        print("{:02d}, " .format(x), end='')
