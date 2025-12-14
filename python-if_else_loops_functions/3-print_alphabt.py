59e2d551112a6ace2473e49916b3642fcefe5688
Pholi658 <totipholiso@gmail.com>
2025-12-14 22:07:25 +0200

    another initial commit

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for x in range(97, 122):
+    if x == 101 and x == 113:
+        continue
+    else:
+        print(chr(x), end="")
---
+++ python-if_else_loops_functions/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+number = 2345
+last_digit = number % 10
+
+print(f"{last_digit} is the last namba")
