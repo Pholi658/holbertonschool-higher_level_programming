d3a45a5a77109c4114c4e31f0998d3143cdf88bc
Pholiso Toti <84088467+Pholi658@users.noreply.github.com>
2026-01-19 00:04:15 +0200

    Enhance docstring for append_write function
    Added docstring for the append_write function to clarify its purpose and parameters.

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Defines function that appends to txt file"""
 
 def append_write(filename="", text=""):
+   """function appends text to file
+    Args:
+        filename: file to append to
+        text: text to append
+    Return:
+        returns num of characters of text
+    """ 
+    
     with open(filename,'a',encoding="utf-8") as f:
         """function appends text to file
         Args:
