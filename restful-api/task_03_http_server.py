4483999ce49387cc92e7a30c69a07534f3a1345c
Pholi658 <totipholiso@gmail.com>
2026-01-25 18:43:20 +0200

    No4 fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,8 +4,9 @@ import http.server
 import socketserver
 import json
 
-
 PORT = 8000
+
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
@@ -23,23 +24,20 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
                 "age": 30,
                 "city": "New York"
             }
-            self.wfile.write(json.dumps(response_data).encode('utf-8'))
+            self.wfile.write(json.dumps(response_data).encode("utf-8"))
+
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "status": "OK"
-            }"""
             self.wfile.write(b"OK")
+
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            """response_data = {
-                "error": "Endpoint not found"
-            }"""
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Not Found")
+
 
 Handler = MyHandler
 
