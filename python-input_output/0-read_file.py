622bef136815adc9b09af0bbd71fa9b33663b412
Pholi658 <totipholiso@gmail.com>
2026-01-25 13:38:39 +0200

    first commits

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-"""function that reads a text file (UTF8) and prints it to stdout"""
+
+"""a function that reads a text file (UTF8) and prints it to stdout"""
 
 def read_file(filename=""):
-    """Function returns a UTF* text file
+  """Function returns a UTF* text file
     Args:
-        filename: name of file to read
+        filename: placeholder of file to read
     Return:
         UTF8 text file
     """
     with open(filename,'r',encoding="utf-8") as f:
-       text = f.read()
-       print(text)
-    return text
+         textfile = f.read()
+    print(textfile)
--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,16 +1,20 @@
-#!/usr/bin/python3
-"""Defines function that appends to txt file"""
+#!/usr/bin/python
+
+"""appends a string at the end of a text file (UTF8) and returns the number of characters added"""
+
 
 def append_write(filename="", text=""):
-   """function appends text to file
-    Args:
-        filename: file to append to
-        text: text to append
-    Return:
-        returns num of characters of text
-    """ 
+
     
-    with open(filename,'a',encoding="utf-8") as f:
+    """function writes text to file
+    Args:
+        filename: name of file to write to
+        text: text to write
+    Return:
+        returns number of text characters
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
         f.write(text)
-        wordlen = len(text)
-        return wordlen
+    alen = len(text)
+    return alen
